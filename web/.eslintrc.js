6914d51a8fb62efdc07e4bfc6f9f9d263cb0a8dc
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-11-05 22:59:08 -0300

    web:add import helpers eslint

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -15,11 +15,18 @@ module.exports = {
 		ecmaVersion: 2018,
 		sourceType: 'module',
 	},
-	plugins: ['react', 'prettier'],
+	plugins: ['react', 'prettier', 'eslint-plugin-import-helpers'],
 	rules: {
 		'prettier/prettier': ['error', { endOfLine: 'auto' }],
 		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
 		'react/jsx-props-no-spreading': 'off',
 		'react/react-in-jsx-scope': 'off',
+		'import-helpers/order-imports': [
+			'warn',
+			{
+				newlinesBetween: 'always',
+				groups: ['module', ['parent', 'sibling', 'index']],
+			},
+		],
 	},
 };
