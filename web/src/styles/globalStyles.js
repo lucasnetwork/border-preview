401db092c676b7f55501c145e83fb483cbb541c3
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-11-06 13:05:23 -0300

    web:add Roboto font

--- web/src/styles/globalStyles.js
+++ web/src/styles/globalStyles.js
@@ -1,21 +1,28 @@
 import { createGlobalStyle } from 'styled-components';
 
+import RobotoRegular from '../fonts/Roboto-Regular.ttf';
+
 export default createGlobalStyle`
+	@font-face {
+		font-family: 'Roboto';
+		src: url(${RobotoRegular}) ;
+		font-weight: 400;
+		font-style: normal;
+  }
 	*{
 		margin:0;
 		padding:0;
 		outline:0;
-		box-sizing:border-box
-	}
-
-	html,body,#root{
-		width:100%;
-		height:100%;
+		box-sizing:border-box;
+		font-size:16px;
+		font-family:'Roboto'
 	}
 
 	.App{
-		width:100%;
-		height:100%;
-		background-color:#E0E2DB
+		display:flex;
+		flex-direction:column;
+		width:100vw;
+		height:100vh;
+		background-color:#E0E2DB;
 	}
 `;
